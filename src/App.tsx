3f26dace85fe35b7ba0822b420fd863def53be7b
Jademir de Moura <jademir@policiacivil.pe.gov.br>
2023-09-15 13:40:24 -0300

    text areas and video area

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,56 @@
+import { Github } from "lucide-react";
 import { Button } from "./components/ui/button";
+import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
 
 export function App() {
 
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <div className="px-6 py-3 flex items-center justify-between border-b">
-        <h1>upload.ai</h1>
+        <h1 className="text-xl font-bold">upload.ai</h1>
 
-        <div>
-          <span>Project from Rocketseat's NLW 💻 </span>
-          <Button>Github</Button>
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Project from Rocketseat's NLW 💻
+          </span>
+          <Separator orientation="vertical" className="h-6" />
+          <Button variant="link" size="sm" className="border-2 border-primary">
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </Button>
         </div>
       </div>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Inclua o prompt para a IA" 
+            />
+            <Textarea
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Resultado gerado pela IA"
+              readOnly 
+            />
+          </div>
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: você pode utilizar a variável <code className="text-primary">{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.
+          </p>
+        </div>
+        <aside className="w-80 space-y-6">
+          <form className="space-y-6">
+            <label
+              htmlFor="video"
+              className="boerder flex rounded-md aspect-video cursor-pointer border-2 border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+            >
+              Carregar vídeo
+            </label>
+            <input type="file" id="video" accept="video/mp4" className="sr-only" />
+          </form>
+        </aside>
+      </main>
     </div>
   )
 }
